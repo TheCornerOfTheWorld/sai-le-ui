1f6ac65527adc4e04c1c4ae728c268128615ea5d
lkx <851657005@qq.com>
2024-01-30 09:45:03 +0800

    chore: 调整声明文件配置

--- packages/shims-vue.d.ts
+++ packages/shims-vue.d.ts
@@ -1,5 +1,5 @@
 declare module "*.vue" {
-  import type { DefineComponent, Plugin } from "vue";
+  import type { DefineComponent, Plugin } from "vue-demi";
 
   const component: DefineComponent<{}, {}, any> & Plugin;
   export default component;
--- scripts/rollup.config.ts
+++ scripts/rollup.config.ts
@@ -3,17 +3,23 @@ import vue from "rollup-plugin-vue";
 import scss from "rollup-plugin-scss";
 import typescript from "rollup-plugin-typescript2";
 import { defineConfig } from "rollup";
-
 import dartSass from "sass";
 
 const input = "packages/core/index.ts";
-const overrides = {
-  compilerOptions: { declaration: true }, // 是否创建 typescript 声明文件
+
+const tsPluginOpt = {
+  tsconfigOverride: {
+    compilerOptions: { declaration: true }, // 是否创建 typescript 声明文件
+  },
+  // include: ["packages/core"],
   exclude: [
     // 排除项
+    "metadata/*",
+    "vite.config.ts",
     "node_modules",
   ],
 };
+
 export default defineConfig([
   {
     input,
@@ -23,7 +29,7 @@ export default defineConfig([
     ],
     plugins: [
       scss({ include: /\.scss$/, sass: dartSass }),
-      typescript({ tsconfigOverride: overrides }),
+      typescript(tsPluginOpt),
       esbuild({
         include: /\.[jt]s$/,
         minify: process.env.NODE_ENV === "production",
